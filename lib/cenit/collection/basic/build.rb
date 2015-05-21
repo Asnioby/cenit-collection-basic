0f80959d8c527848016f920391b751cfb149a3fe
Asnioby Hdez <asnioby@gmail.com>
2015-05-21 13:55:06 -0400

    Initial commit to cenit-collection-basic.

---
+++ lib/cenit/collection/basic/build.rb
@@ -0,0 +1,14 @@
+require 'cenit/collection/basic/version'
+require 'cenit/collection/base/build'
+
+module Cenit
+  module Collection
+    module Basic
+      class Build < Cenit::Collection::Base::Build
+        def initialize
+          super(__dir__)
+        end
+      end
+    end
+  end
+end
